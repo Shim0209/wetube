e269a0ca34c8a5df5a9b831d374bdb284eee2ebf
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2021-04-12 22:07:37 +0900

    Add middleware

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
  
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send('Hello from home!');
+const handleHome = (req, res) => {
+    res.send("Hi from home!");
+}
+const handleProfile = (req, res) => res.send('You are a my profile!');
 
-const handleProfile = (req, res) => res.send('You are a my profile!!');
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
