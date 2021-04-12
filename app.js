7793934bb0eace5e42476e97d3272683fab07748
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2021-04-12 23:06:51 +0900

    Add init.js and router.js

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
- 
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => {
     res.send("Hi from home!");
@@ -21,7 +18,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
+app.use("/user", userRouter);
 
-app.listen(PORT, handleListening);
+export default app;
